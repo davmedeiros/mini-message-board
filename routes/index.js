54ad353a6a396fe895a4f8ad2682c48b659bb526
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-09-01 11:43:40 +0100

    Refactor template code with modern JavaScript

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 const messages = [
   {
@@ -15,7 +15,7 @@ const messages = [
 ];
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
 
