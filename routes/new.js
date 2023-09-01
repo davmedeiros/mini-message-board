1c7edf88931017fde063b45067d48f5449ce06aa
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-09-01 10:58:18 +0100

    Move "/new" route to index

--- routes/new.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res, next) => {
-  // res.send('Working');
-  res.render('form', { title: 'New Message' });
-});
-
-module.exports = router;
